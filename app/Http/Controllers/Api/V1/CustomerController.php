67074b11dcccf311ff1a0c576a4b2996b1e41936
Walid <walid.r.rashed@gmail.com>
2022-07-28 19:24:13 +0300

    feat: adding customers via api

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -5,12 +5,11 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Filters\V1\CustomersFilter;
-
+use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
 class CustomerController extends Controller
 {
     /**
@@ -51,7 +50,7 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     /**
