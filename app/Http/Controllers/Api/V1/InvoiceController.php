0f128250ebc143fd83cfb400a78c0bf8804d83c1
Walid <walid.r.rashed@gmail.com>
2022-07-28 15:28:36 +0300

    chore: creating and implimenting Resource and Collection

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -7,6 +7,8 @@ use App\Models\Invoice;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\InvoiceResource;
+use App\Http\Resources\V1\InvoiceCollection;
 
 class InvoiceController extends Controller
 {
@@ -17,7 +19,7 @@ class InvoiceController extends Controller
      */
     public function index()
     {
-        return Invoice::all();
+        return new InvoiceCollection(Invoice::paginate());
     }
 
     /**
@@ -49,7 +51,7 @@ class InvoiceController extends Controller
      */
     public function show(Invoice $invoice)
     {
-        //
+        return new InvoiceResource($invoice);
     }
 
     /**
